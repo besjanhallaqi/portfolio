7ce57a268fe5fa5f5f383c53e1a2c6aac9e8506d
bh41352 <bh41352@ubt-uni.net>
2024-10-27 19:35:02 +0100

    added next seo

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,47 @@
-import Head from "next/head";
+import { DefaultSeo } from "next-seo";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <Head>
-        <title>Besjan Hallaqi - Portfolio</title>
-        <meta
-          name="description"
-          content="Discover the portfolio of Besjan Hallaqi, showcasing your skills in industry through types of work."
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="og:title" content="Besjan Hallaqi - Portfolio" />
-        <meta
-          property="og:description"
-          content="Discover the portfolio of Besjan Hallaqi, showcasing skills in industry work."
-        />
-        <meta property="og:image" content="/images/me.jpg" />
-        <meta property="og:url" content="https://besjanhallaqi.com" />
-        <meta property="og:type" content="website" />
-        <meta charSet="UTF-8" />
-        <meta http-equiv="Content-Language" content="en" />
-        <meta
-          name="keywords"
-          content="Besjan Hallaqi, Portfolio, Developer, Skills, Front-end"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <DefaultSeo
+        title="Besjan Hallaqi - Portfolio"
+        description="Discover the portfolio of Besjan Hallaqi, showcasing skills in industry work."
+        openGraph={{
+          type: "website",
+          url: "https://besjanhallaqi.com",
+          title: "Besjan Hallaqi - Portfolio",
+          description:
+            "Discover the portfolio of Besjan Hallaqi, showcasing skills in industry work.",
+          images: [
+            {
+              url: "/images/me.jpg",
+              width: 600,
+              height: 600,
+              alt: "Besjan Hallaqi Portfolio",
+            },
+          ],
+        }}
+        additionalMetaTags={[
+          {
+            name: "viewport",
+            content: "width=device-width, initial-scale=1",
+          },
+          {
+            charSet: "UTF-8",
+          },
+          {
+            httpEquiv: "Content-Language",
+            content: "en",
+          },
+          {
+            name: "keywords",
+            content: "Besjan Hallaqi, Portfolio, Developer, Skills, Front-end",
+          },
+        ]}
+      />
       <Component {...pageProps} />
-    </div>
+    </>
   );
 }
 
